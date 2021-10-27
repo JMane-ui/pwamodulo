d498c914577df067d400460a8d7ce2874dd19987
JMane-ui <manuelfrancoesquivel@gmail.com>
2021-10-27 00:06:29 -0500

    sw correct

--- sw.js
+++ sw.js
@@ -75,29 +75,3 @@ self.addEventListener('fetch', e => {
             })
     );
 })
-
-self.addEventListener('push', function(event) {
-    if (!(self.Notification && self.Notification.permission === 'granted')) {
-      return;
-    }
-  
-    var data = {};
-    if (event.data) {
-      data = event.data.json();
-    }
-    var title = data.title || "Something Has Happened";
-    var message = data.message || "Here's something you might want to check out.";
-    var icon = "img/circle-cropped.png";
-  
-    var notification = new self.Notification(title, {
-      body: message,
-      tag: 'simple-push-demo-notification',
-      icon: icon
-    });
-  
-    notification.addEventListener('click', function() {
-      if (clients.openWindow) {
-        clients.openWindow('https://example.blog.com/2015/03/04/something-new.html');
-      }
-    });
-  });
