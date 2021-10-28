29004ccd21f9b0027162703080739ef82da369fd
pasabaa <sanchez.ballesteros.pablo@gmail.com>
2021-10-28 18:07:21 -0500

    se agrego evento push

--- sw.js
+++ sw.js
@@ -71,7 +71,7 @@ self.addEventListener('activate', e => {
                 self.clients.claim(); //activar cache en todos los clientes
             })
     )
-})
+});
 
 //fetch
 self.addEventListener('fetch', e => {
@@ -84,4 +84,25 @@ self.addEventListener('fetch', e => {
                 return fetch( e.request );
             })
     );
-})
+});
+
+self.addEventListener('push', function(e) {
+    var options = {
+      body: 'This notification was generated from a push!',
+      icon: 'img/favicon.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2'
+      },
+      actions: [
+        {action: 'explore', title: 'Explore this new world',
+          icon: 'images/checkmark.png'},
+        {action: 'close', title: 'Close',
+          icon: 'images/xmark.png'},
+      ]
+    };
+    e.waitUntil(
+      self.registration.showNotification('Hello world!', options)
+    );
+  });
